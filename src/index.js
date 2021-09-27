63fb802757028523feb9c725104321f091f87be5
Alex <powershot317@gmail.com>
2021-09-27 12:23:52 +0300

    add strict mode

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 import App from './_components/App/App';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
